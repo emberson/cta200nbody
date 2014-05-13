66b952fd43b4d104289f591901b2a98ca4f31cb0
Ryan Cloutier <cloutier@moose.cita.utoronto.ca>
2014-05-13 16:07:29 -0400

    Added pencilbackward.

--- cube.f90
+++ cube.f90
@@ -8,7 +8,7 @@ program cube
     implicit none
 
     !! Simulation parameters
-    integer, parameter :: ngrid = 2
+    integer, parameter :: ngrid = 4
     integer, parameter :: ncube = 2
     integer, parameter :: np = 32
     integer, parameter :: timesteps = 3
@@ -35,6 +35,9 @@ program cube
     complex, dimension(ngrid*ncube/2+1, npen, ncube, npen) :: crhox[ncube, ncube, *]
     complex, dimension(npen, ncube, ncube, npen, ngrid/2+1) :: crhoy[ncube, ncube, *]
     complex, dimension(npen, ncube, ncube, ngrid/2+1, npen) :: crhoz[ncube, ncube, *]
+
+    complex, dimension(ngrid*ncube/2+1, npen, ncube, npen) :: crhox_copy[ncube, ncube, *]
+
 
     !! Temporary coarrays needed in the pencil routines
     complex, dimension(ngrid/2+1, npen, ncube, npen) :: crhotmpyxglobal[ncube, ncube, *]
@@ -79,9 +82,9 @@ program cube
     call setup_kernels
     call initial_conditions
 
-    do it = 1, timesteps
+    do it = 1, 1!timesteps
 
-        call calculate_rho
+!        call calculate_rho
         call poisson_solve
         call update_particles
         call send_particles
@@ -179,7 +182,7 @@ contains
         !
 
         implicit none
-        integer i,j,k, jStart,jStop,IMG
+        integer i,j,k, jStart,jStop,IMG,ind
         complex, dimension(ngrid/2+1, npen, ncube, npen) :: temp
         !
         ! First unpack the cubic representation of rho3 into a pencil representation in rhox.
@@ -188,28 +191,32 @@ contains
         !
         IMG=this_image()
         
+        ind = (IMG-1)*ngrid**3 + 1
         do i=1,ngrid
            do  j=1,ngrid
               do k=1,ngrid
-                 rho(k,j,i)=(IMG-1)*ncube**3+(i-1)*ngrid**2+(j-1)*ngrid+k
+                 rho(k,j,i)=ind
+                 ind = ind + 1!nin-1)*ncube**3+(i-1)*ngrid**2+(j-1)*ngrid+k
               enddo
            enddo
         enddo
         rho3=rhol
         
-        sync all
+        !sync all
         !call sleep(IMG)
         !write(*,*) 'IMG rho3 = ',IMG,' rho3 =' ,rho3
-        
+        !call sleep(num_images()-IMG)
+
         ! GO FROM RHO3 TO RHOX
         rhox = -IMG
         do i = 1,ncube
            rhox((i-1)*ngrid+1:i*ngrid,:,:,:)=rho3(:,:,:,:,mycoord(2))[i,mycoord(1),mycoord(3)]
         enddo
         sync all
-        !call sleep(IMG)
-        !write(*,*)  'IMG rhox = ',IMG,' rhox = ', rhox
-        
+        call sleep(IMG)
+        write(*,*)  'IMG rhox = ',IMG,' rhox = ', rhox
+        call sleep(num_images()-IMG)
+       
        ! call fftvec(rhox, ngrid*ncube, ngrid**2/ncube, 1)
 
         !
@@ -218,16 +225,24 @@ contains
         !
         
         crhox = cmplx(rhox(::2,:,:,:), rhox(2::2,:,:,:))
+ 
+!        call sleep(IMG)       
+!        write(*,*)  'IMG crhox* = ',IMG,' crhox* = ', crhox
+!        call sleep(num_images()-IMG)
+!        sync all
         
-        !write(*,*)  'IMG crhox = ',IMG,' crhox = ', crhox
-        sync all
+
+        !complex, dimension(ngrid*ncube/2+1, npen, ncube, npen) :: crhox[ncube, ncube, *]
+        !complex, dimension(npen, ncube, ncube, npen, ngrid/2+1) :: crhoy[ncube, ncube, *]
+        !complex, dimension(npen, ncube, ncube, ngrid/2+1, npen) :: crhoz[ncube, ncube, *]
+
         ! GO FROM CRHOX -> CRHOY
         crhoy=0
         !jStart=(mycoord(3)-1)*ngrid/2+1
         jStart=1
         !jStop=ngrid*mycoord(3)/2
         jStop=ngrid/2
-        write(*,*)jStart, ' <--jStart  jStop--> ',jStop
+        !write(*,*)jStart, ' <--jStart  jStop--> ',jStop
         if (mycoord(3) == ncube) jStop= jStop+1
         do i = 1,ncube
            temp=crhox((mycoord(3)-1)*ngrid/2+1:mycoord(3)*ngrid/2+1,:,:,:)[i,mycoord(1),mycoord(2)]
@@ -236,9 +251,10 @@ contains
               crhoy(:,:,i,:,j)=temp(j,:,:,:)!crhox(j,:,:,:)[i,mycoord(1),mycoord(2)]
            enddo
         enddo
-        sync all
-        call sleep(IMG)
-        write(*,*) 'IMG crhoy = ',IMG,' crhoy = ', crhoy
+!        sync all
+!        call sleep(IMG)
+!        write(*,*) 'IMG crhoy = ',IMG,' crhoy = ', crhoy
+!        call sleep(num_images()-IMG)
         !call cfftvec(crhoy, ngrid*ncube, ngrid*(ngrid/2+1)/ncube, 1)
 
         !
@@ -259,9 +275,12 @@ contains
         !
 
         implicit none
+        integer i,j,k, jStart,jStop,IMG
+        complex, dimension(npen,ncube,npen,ngrid/2+1) :: temp
 
         call cfftvec(crhoz, ngrid*ncube, ngrid*(ngrid/2+1)/ncube, -1)
 
+!        crhox_copy(:,:,:,:) = crhox(:,:,:,:)
         !
         ! Transpose from crhoz to crhoy so that pencils have their longest axis in y and 
         ! their shortest in z. Pencils are stacked along the z axis first and then x.
@@ -269,16 +288,45 @@ contains
 
         !! PUT CODE HERE FOR CRHOZ -> CRHOY
 
-        call cfftvec(crhoy, ngrid*ncube, ngrid*(ngrid/2+1)/ncube, -1)
+        !call cfftvec(crhoy, ngrid*ncube, ngrid*(ngrid/2+1)/ncube, -1)
 
         !
         ! Transpose from crhoy to crhox so that pencils have their longest axis in x and 
         ! their shortest in z. Pencils are stacked along y first and then z.
         !
     
+        
         !! PUT CODE HERE FOR CRHOY -> CRHOX
+        IMG=this_image()
+        jStart=1
+        jStop=ngrid/2
+        !write(*,*)jStart, ' <--jStart  jStop--> ',jStop
+        !if (mycoord(3) == ncube) jStop= jStop+1
+        do i = 1,ncube
+           !temp=crhoy(:,:,:,:,(mycoord(3)-1)*ngrid/2+1:mycoord(3)*ngrid/2+1)[mycoord(2),mycoord(3),i]
+           
+           !do j = jStart, jStop
+            !  crhox(j,:,:,:)=temp(:,:,i,:,j)!crhox(j,:,:,:)[i,mycoord(1),mycoord(2)]
+              
+           !enddo
+           temp = crhoy(:,:,myCoord(1),:,:)[mycoord(2),mycoord(3),i]
+           if (i == ncube) jStop = jStop +1
+           do j = jStart,jStop
 
-        call fftvec(crhox, ngrid*ncube, ngrid**2/ncube, -1)
+              crhox((i-1)*ngrid/2+j,:,:,:) = temp(:,:,:,j)
+           enddo
+        enddo
+
+        sync all
+!        crhox = crhox - crhox_copy
+
+        sync all
+        call sleep(IMG)
+        write(*,*)  'IMG crhox = ',IMG,' crhox = ', crhox
+        !call sleep(num_images()-IMG)
+
+
+        !call fftvec(crhox, ngrid*ncube, ngrid**2/ncube, -1)
 
         rhox(::2,:,:,:) = real(crhox)
         rhox(2::2,:,:,:) = aimag(crhox)
